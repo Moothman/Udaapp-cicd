9f045e7dc035760dab056cbfadd114209aea4c57
Moothman <mosman.esmail@gmail.com>
2022-08-01 06:14:24 +0200

    another fixing1

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -7,7 +7,7 @@ export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
 export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
 
 export TYPEORM_HOST=udapeople-db.cybkxaisw50a.us-west-2.rds.amazonaws.com
-export TYPEORM_PORT=5532
+export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=Ozxj9sz17LruzsUO5Gj9
 export TYPEORM_DATABASE=glee
