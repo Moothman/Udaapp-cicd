7fe8c7e31dc6047bd1a603665bc9592f64c0f244
Moothman <mosman.esmail@gmail.com>
2022-08-02 06:45:48 +0200

    fix bug2

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -1,10 +1,17 @@
 export NODE_ENV=production
 export VERSION=1
-
 export TYPEORM_CONNECTION=postgres
-export TYPEORM_MIGRATIONS_DIR=./migrations
-export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
-export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_MIGRATIONS_DIR=./dist/migrations
+export TYPEORM_ENTITIES=./dist/modules/domain/**/*.entity(.ts,.js)
+export TYPEORM_MIGRATIONS=./dist/migrations/*(.ts,.js)
+
+# export NODE_ENV=production
+# export VERSION=1
+
+# export TYPEORM_CONNECTION=postgres
+# export TYPEORM_MIGRATIONS_DIR=./migrations
+# export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+# export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
 
 export TYPEORM_HOST=udapeople-db.cybkxaisw50a.us-west-2.rds.amazonaws.com
 export TYPEORM_PORT=5432
