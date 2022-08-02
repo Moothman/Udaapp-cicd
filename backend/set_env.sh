cfc577cf838db933d3bfddb2041e522b1306f479
Moothman <mosman.esmail@gmail.com>
2022-08-02 23:04:07 +0200

    some changes on env.V

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -1,23 +1,23 @@
 export NODE_ENV=production
 export VERSION=1
 export TYPEORM_CONNECTION=postgres
-export TYPEORM_MIGRATIONS_DIR=./dist/migrations
-export TYPEORM_ENTITIES=./dist/modules/domain/**/*.entity(.ts,.js)
-export TYPEORM_MIGRATIONS=./dist/migrations/*(.ts,.js)
-
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_HOST=udapeople-db.cybkxaisw50a.us-west-2.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=Ozxj9sz17LruzsUO5Gj9
+export TYPEORM_DATABASE=glee
 # export NODE_ENV=production
 # export VERSION=1
 
 # export TYPEORM_CONNECTION=postgres
 # export TYPEORM_MIGRATIONS_DIR=./migrations
 # export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
-# export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+# export TYPEORM_MIGRATIONS=./dist/migrations/*{.ts,.js}
 
-export TYPEORM_HOST=udapeople-db.cybkxaisw50a.us-west-2.rds.amazonaws.com
-export TYPEORM_PORT=5432
-export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD=Ozxj9sz17LruzsUO5Gj9
-export TYPEORM_DATABASE=glee
+
 
 # export TYPEORM_HOST=udapeople-db.cybkxaisw50a.us-west-2.rds.amazonaws.com
 # export TYPEORM_PORT=5432
